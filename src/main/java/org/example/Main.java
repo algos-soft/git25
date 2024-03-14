a7b4abdafd3ed049b8a4db356502827253a5fb9e
Alex <info@algos.it>
2024-03-14 15:52:08 +0100

    v5

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import it.algos.vbase.service.*;
+
+public class Main {
+
+    MyService myService;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
